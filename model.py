a820d13a60c2d8998b44ee99e12a7ed14e34ba1e
Tayfun Kok, Ph.D. <102910991+drtayfunkok@users.noreply.github.com>
2025-02-18 01:50:15 +0000

    Update model.py

--- model.py
+++ model.py
@@ -6,6 +6,7 @@
 
 import openai
 import os
+from preprocessing import extract_customer_responses  # Import the function
 
 # Loads API key from env variable
 openai.api_key = os.getenv("OPENAI_API_KEY")  
