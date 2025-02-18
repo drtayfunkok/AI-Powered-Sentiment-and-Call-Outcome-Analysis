4f8b5c3bc0d461068c05c1f5aee982a5be810080
Tayfun Kok, Ph.D. <102910991+drtayfunkok@users.noreply.github.com>
2025-02-18 05:49:47 +0000

    Update test_api.py

--- Test/test_api.py
+++ Test/test_api.py
@@ -1,1 +1,14 @@
+import unittest
+import requests
+
+class TestAPI(unittest.TestCase):
+    API_URL = "http://127.0.0.1:5000"
+
+    def test_upload_file(self):
+        files = {'file': open("sample_transcript.txt", 'rb')}
+        response = requests.post(f"{self.API_URL}/", files=files)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
 
